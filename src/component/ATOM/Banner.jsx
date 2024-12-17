969730c247b5fce833f8d054e40e4d1b45bb1720
satria arya diva <satriaarya125@gmail.com>
2024-12-18 01:05:37 +0700

    z

--- src/component/ATOM/Banner.jsx
+++ src/component/ATOM/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = ({children} ) => {
     <Fragment>
       <div
         className="relative my-10 w-full h-96 bg-no-repeat bg-cover bg-center object-center"
-        style={{ backgroundImage: "url(' /assets/assets/Banner.jpeg')" }}
+        style={{ backgroundImage: "url('/assets/Banner.jpeg')" }}
       >
         <div className="relative inset-0 bg-black w-full h-full bg-opacity-60 flex flex-col items-center justify-center text-center">
         <h1 className="text-4xl font-bold text-white mb-4 animate__animated animate__fadeInDown">Find Your Perfect Ride</h1>
