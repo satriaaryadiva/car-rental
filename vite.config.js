bb68a077f3dfb4fb1d2c615fee2275f0eb953716
satria arya diva <satriaarya125@gmail.com>
2024-12-28 00:51:37 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
-  root: '.',
-  server: {
-    port: 5179,  // Ganti port jika perlu
-    host: 'localhost',  // Pastikan host diatur dengan benar
-    open: true,  // Membuka otomatis di browser saat server dijalankan
-    hmr: true,  // Mengaktifkan hot module replacement (HMR) untuk pengembangan yang lebih cepat
-  },
-})
+  root: './', // Lokasi root
+  publicDir: 'public', // Folder public
+});
