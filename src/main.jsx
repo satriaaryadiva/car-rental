d96fc8fe0c18e638aaa303ed1c94ef4b4fbf16d8
satria arya diva <satriaarya125@gmail.com>
2024-12-28 01:15:35 +0700

    test deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,47 @@
-// src/main.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Import global CSS
-import App from './App.jsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import MainPage from './component/PAGE/MainPage';
+import RentalForm from './component/FRAGMENT/RentalForm';
+import PaymentQR from './component/FRAGMENT/QRPayment';
+import store from './redux/store.jsx';
+import ErrorrPage from './component/PAGE/ErrorPage';
+import Banner from './component/ATOM/Banner.jsx';
+import { Analytics } from '@vercel/analytics/next';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
+const routes = [
+  {
+    path: '/',
+    element: <MainPage />,
+    errorElement: <div>Error</div>,
+  },
+  {
+    path: '/rent/:carId',
+    element: <RentalForm />,
+    errorElement: <ErrorrPage/>,
+  },
+  {
+    path: '/payment/:carId',
+    element: <PaymentQR />,
+    errorElement: <di>Evrror</di>,
+
+  },{
+    path: '/test',
+    element: <Banner/>,
+    errorElement:<ErrorrPage></ErrorrPage>
+  }
+];
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>  
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Analytics/>
+    </Provider> 
+    
   </React.StrictMode>
 );
