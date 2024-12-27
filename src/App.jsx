cd30cdf7a1c107c8977c0d1a6c0439c6fa977406
satria arya diva <satriaarya125@gmail.com>
2024-12-28 00:08:32 +0700

    add use effect for analitik

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 // src/app.js
- 
+
 import { Provider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import store from './redux/store.jsx'; // Import store Redux
-import routes from './routes'; // Pisahkan rute ke file tersendiri
+import { RouterProvider, createBrowserRouter, useLocation } from 'react-router-dom';
+import store from './redux/store.jsx'; // Import Redux store
+import routes from './routes'; // Import konfigurasi rute
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect } from 'react';
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(`Navigated to ${location.pathname}`); // Debugging navigasi
+  }, [location]);
+
   return (
     <Provider store={store}>
       <RouterProvider router={createBrowserRouter(routes)} />
